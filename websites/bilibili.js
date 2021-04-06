f67bb6cc9dd774442a906cf12954cd409d7d3514
王戈wg <69033087+nc-animation@users.noreply.github.com>
2021-04-06 14:14:21 +0800

    Create bilibili.js

---
+++ websites/bilibili.js
@@ -0,0 +1,32 @@
+let { URL } = require('url')
+let fetch = require('node-fetch')
+let { JSDOM } = require('jsdom')
+
+module.exports = {
+  test(url) {
+    let parsed = new URL(url)
+    return parsed.hostname.match('t.bilibili.com')
+  },
+
+  async process(url) {
+    let res = await fetch(url)
+    let html = await res.text()
+    let document = new JSDOM(html).window.document
+
+    let title = document.querySelector('h1.title').textContent
+    let author = document.querySelector('a.up-name').textContent
+    let content = document.querySelector('div.article-holder')
+
+    return {
+      title,
+      author,
+      dom: content
+    }
+
+  },
+
+  samples: [
+    'https://t.bilibili.com/510166072378397988',
+    'https://t.bilibili.com/h5/dynamic/detail/510166072378397988'
+  ]
+}
